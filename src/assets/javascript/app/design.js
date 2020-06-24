3db7afb1bd83e4ede2706255909e61981f9c4c00
thomas.duvinage <thomas.duvinage@utbm.fr>
2020-06-24 23:49:03 +0200

    delete sliders

--- src/assets/javascript/app/design.js
+++ src/assets/javascript/app/design.js
@@ -3,29 +3,13 @@ var app = app || {};
 var Design = {};
 
 Design.init = function () {
-    document.addEventListener('DOMContentLoaded', function () {
-        var elems = document.querySelectorAll('.sidenav');
-        var instances = M.Sidenav.init(elems, {});
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+
+    $("#panelControl").load("assets/html/ControlPanel.html", function () {
+        app.controlPanel.init();
     });
-
-
-    $(document).ready(function () {
-        $("#panelControl").load("assets/html/ControlPanel.html");
-    });
-
-    var pipsSlider = document.getElementById('slider-pips');
-
-    noUiSlider.create(pipsSlider, {
-        range: {
-            min: 0,
-            max: 100
-        },
-        start: [50],
-        pips: { mode: 'count', values: 5 }
-    });
-
 }
-
 
 
 app.Design = Design;
